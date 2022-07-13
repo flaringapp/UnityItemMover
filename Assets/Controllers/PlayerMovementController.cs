6f2844b77c03c0c8766b2747d39d4819d37c98d5
User <User@DESKTOP-HVULQQV>
2022-07-13 22:11:28 +0300

    Implement sprint mechanics

--- Assets/Controllers/PlayerMovementController.cs
+++ Assets/Controllers/PlayerMovementController.cs
@@ -9,15 +9,26 @@ namespace Controllers
         [SerializeField] private CharacterController playerController;
 
         [SerializeField] private float movementSpeed;
+        [SerializeField] private float sprintSpeed;
         [SerializeField] private float jumpHeight;
         [SerializeField] private float gravity = Physics.gravity.y;
 
+        private float _jumpVelocity;
+
         private Vector3 _moveVelocity = Vector3.zero;
+
+        private float _currentSpeed;
+        private bool _isSprinting;
         private bool _hasDoubleJumped;
 
         private void Start()
         {
+            _jumpVelocity = Mathf.Sqrt(jumpHeight * gravity * -2f);
+            _currentSpeed = movementSpeed;
+
             StartCoroutine(MovePlayer());
+            StartCoroutine(ObserveSprint());
+            StartCoroutine(SprintSpeedAdjustment());
         }
 
         private IEnumerator MovePlayer()
@@ -45,8 +56,8 @@ namespace Controllers
 
         private void ProcessSurfaceMovement()
         {
-            var x = Input.GetAxisRaw("Horizontal") * movementSpeed;
-            var z = Input.GetAxisRaw("Vertical") * movementSpeed;
+            var x = Input.GetAxisRaw("Horizontal") * _currentSpeed;
+            var z = Input.GetAxisRaw("Vertical") * _currentSpeed;
 
             var move = playerTransform.right * x + playerTransform.forward * z;
             move.y += _moveVelocity.y;
@@ -61,8 +72,34 @@ namespace Controllers
             if (_hasDoubleJumped && !isGrounded) return;
 
             _hasDoubleJumped = !isGrounded;
-            
-            _moveVelocity.y = Mathf.Sqrt(jumpHeight * gravity * -2f);
+
+            _moveVelocity.y = _jumpVelocity;
+        }
+
+        private IEnumerator ObserveSprint()
+        {
+            while (true)
+            {
+                yield return CoroutineUtils.WaitForKeyDown(KeyCode.LeftShift);
+                _isSprinting = true;
+                yield return CoroutineUtils.WaitForKeyUp(KeyCode.LeftShift);
+                _isSprinting = false;
+                yield return null;
+            }
+        }
+
+        private IEnumerator SprintSpeedAdjustment()
+        {
+            while (true)
+            {
+                var targetSpeed = _isSprinting ? sprintSpeed : movementSpeed;
+                while (Mathf.Abs(targetSpeed - _currentSpeed) > 0.01f)
+                {
+                    _currentSpeed = Mathf.Lerp(_currentSpeed, targetSpeed, 0.1f);
+                    yield return null;
+                }
+                yield return null;
+            }
         }
     }
 }
